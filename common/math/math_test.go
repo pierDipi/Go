f41fa13089f077a918bbdcc37ad2a225d594c385
pierDipi <pierangelodipilato@gmail.com>
2019-10-29 21:00:10 +0100

    [common/math] Add tests for MinInt function (#5)
    Signed-off-by: pierdipi <pierangelodipilato@gmail.com>

--- common/math/math_test.go
+++ common/math/math_test.go
@@ -21,3 +21,20 @@ func TestMaxInt(t *testing.T) {
 		assertor.AssertThatInt(max).IsEqualInt(tt[r].expected)
 	}
 }
+
+func TestMinInt(t *testing.T) {
+	tt := []struct {
+		a, b, expected int
+	}{
+		{a: -1, b: -1, expected: -1},
+		{a: -1, b: 0, expected: -1},
+		{a: -100, b: -99, expected: -100},
+		{a: 0, b: 1, expected: 0},
+	}
+	assertor := assert.New(t)
+	var min int
+	for r := range tt {
+		min = MinInt(tt[r].a, tt[r].b)
+		assertor.AssertThatInt(min).IsEqualInt(tt[r].expected)
+	}
+}
